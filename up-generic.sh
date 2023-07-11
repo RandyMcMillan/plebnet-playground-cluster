e3eeac57e8e1d7eb3e2010829f3427cef74e2a92
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-07-11 11:57:37 -0400

    up-generic.sh: mkdir -p volumes

--- up-generic.sh
+++ up-generic.sh
@@ -1,18 +1,23 @@
+#This is for internal testing only
 : ${TRIPLET:=x86_64-linux-gnu}
-: ${bitcoind=5}
-: ${lnd=1}
-: ${tor=1}
+if [ -z "$1" ]; then
+	bitcoind="5";
+	lnd="1";
+	tor="1";
+else
+: ${bitcoind=$1}
+: ${lnd=$1}
+: ${tor=$1}
+fi
 
 $(which python3) plebnet_generate.py TRIPLET=$TRIPLET bitcoind=$bitcoind lnd=$lnd tor=$tor
 
 #Remove
 docker-compose down
-sudo rm -rf volumes
-
 
 #Create Datafile
+mkdir -p volumes
 
-mkdir volumes
 for (( i=0; i<=$bitcoind-1; i++ ))
 do
     mkdir -p volumes/bitcoin_datadir_$i
